0e396657d2a15ea457bb609a233e876d96a1238c
Herminio Vazquez <canimus@gmail.com>
2025-03-15 15:36:03 +0100

    Added all new resources

--- capstone/assets/holidays.py
+++ capstone/assets/holidays.py
@@ -1,13 +1,27 @@
 import dagster as dg
-from dagster_duckdb import DuckDBResource
+import pandas as pd
 
 from ..resources import OpenHolidaysResource
 
 
-@dg.asset
+@dg.asset(kinds={"python"})
 def locations(
     context: dg.AssetExecutionContext,
     holidays: OpenHolidaysResource,
-    database: DuckDBResource,
 ):
-    return holidays.langugages()
+    """
+    Retrieves countries available for holidays
+    """
+    countries = holidays.langugages()
+    df = pd.DataFrame()
+    if len(countries) > 0:
+        df = pd.concat([df, pd.DataFrame(countries)])
+        context.log_event(
+            dg.AssetObservation(
+                asset_key=dg.AssetKey(["locations"]),
+                metadata={
+                    "sample": dg.MetadataValue.md(df.isoCode.head().to_markdown())
+                },
+            )
+        )
+    return dg.Output(value=df, metadata={"dagster/row_count": len(countries)})
